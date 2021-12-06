ee42a78600f42fcf10ebd6101b225e6f4f67363e
Vignesh Prabhu <89976945+Vignesh-codezyng@users.noreply.github.com>
2021-12-06 10:33:07 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let googleSignInVersion = "6.0.2"
 let package = Package(
   name: "GoogleSignIn",
   defaultLocalization: "en",
-  platforms: [.iOS(.v9)],[.macOS(.v10_11)],
+  platforms: [.iOS(.v9)],
   products: [
     .library(
       name: "GoogleSignIn",
